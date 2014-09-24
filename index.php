88adc47dd53ce673f029eaaa754f41a509c63792
Alexandre DeFreitas <abd9@njit.edu>
2014-09-24 15:25:53 -0400

    Typing in and testing Keith's code - Part 2

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
 $row_num = 0;
 
+ini_set('auto_detect_line_endings', TRUE);
 if( ($handle = fopen("csv/mycsv.csv", "r")) !== FALSE ) {
   while( ($row = fgetcsv($handle, 1000, ",")) != FALSE) {
     if( $row_num == 0 ) {
@@ -8,10 +9,20 @@ if( ($handle = fopen("csv/mycsv.csv", "r")) !== FALSE ) {
       print_r( $column_heading );
       $row_num = 1;
     } else {
+      $record = array_combine( $column_heading, $row );
+      $records[] = $record;
+    } // end if-else
+  } // end while
+  
+  fclose( $handle );
 
-    }
+} // end if
+
+foreach($records as $record) {
+  foreach( $record as $key => $value ) {
+    echo $key . ':' . $value . "</br> \n";
   }
+  echo '<hr>';
 }
-
 
 ?>
