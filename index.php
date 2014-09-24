5310831344e5338e7a85de8a8be77c25f4a2c777
Alexandre DeFreitas <abd9@njit.edu>
2014-09-24 15:50:01 -0400

    csv data printed in table (non-object-oriented version)

--- index.php
+++ index.php
@@ -3,10 +3,12 @@ $row_num = 0;
 
 ini_set('auto_detect_line_endings', TRUE);
 if( ($handle = fopen("csv/mycsv.csv", "r")) !== FALSE ) {
+  
+
   while( ($row = fgetcsv($handle, 1000, ",")) != FALSE) {
     if( $row_num == 0 ) {
       $column_heading = $row;
-      print_r( $column_heading );
+      //print_r( $column_heading );
       $row_num = 1;
     } else {
       $record = array_combine( $column_heading, $row );
@@ -18,11 +20,23 @@ if( ($handle = fopen("csv/mycsv.csv", "r")) !== FALSE ) {
 
 } // end if
 
-foreach($records as $record) {
-  foreach( $record as $key => $value ) {
-    echo $key . ':' . $value . "</br> \n";
-  }
-  echo '<hr>';
+echo "<table>";
+
+echo "<tr>";
+foreach($column_heading as $column){
+  echo "<th>" . $column . "</th>";
 }
+echo "</tr>";
+
+
+foreach($records as $record) {
+  echo "<tr>";
+  foreach( $record as $key => $value ) {
+    echo "<td>" . $value . "</td>";
+  }
+  echo "</tr>";
+}
+
+echo "</table>";
 
 ?>
