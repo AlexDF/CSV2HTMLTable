c00fbcab24828eef38e069d5b3808db463c99430
Alexandre DeFreitas <abd9@njit.edu>
2014-09-26 06:31:21 -0400

    temporarily placed code inside static function loadRecords()

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 <html>
 <head>
   <link type="text/css" rel="stylesheet" href="css/style.css"/>
+  
 </head>
 
 <body>
@@ -19,52 +20,13 @@
 </form>
 
 <?php
-//$fileDir = $_FILES[csvUpload]["tmp_name"] or $_FILES[csvDrop]["tmp_name"];
-if($_FILES[csvDrop]["tmp_name"]){
-  $fileDir = $_FILES[csvDrop]["tmp_name"];
-} else {
-  $fileDir = $_FILES[csvUpload]["tmp_name"];
-}
+/*ini_set('display_startup_errors',1);
+ini_set('display_errors',1);
+error_reporting(-1);*/
 
-$row_num = 0;
+include("lib/php/class.php");
 
-ini_set('auto_detect_line_endings', TRUE);
-if( ($handle = fopen($fileDir, "r")) !== FALSE ) {
-  
-
-  while( ($row = fgetcsv($handle, 1000, ",")) != FALSE) {
-    if( $row_num == 0 ) {
-      $column_heading = $row;
-      //print_r( $column_heading );
-      $row_num = 1;
-    } else {
-      $record = array_combine( $column_heading, $row );
-      $records[] = $record;
-    } // end if-else
-  } // end while
-  
-  fclose( $handle );
-
-} // end if
-
-echo '<table>';
-
-echo "<tr>";
-foreach($column_heading as $column){
-  echo "<th>" . $column . "</th>";
-}
-echo "</tr>";
-
-
-foreach($records as $record) {
-  echo "<tr>";
-  foreach( $record as $key => $value ) {
-    echo "<td>" . $value . "</td>";
-  }
-  echo "</tr>";
-}
-
-echo "</table>";
+Records::loadRecords();
 
 ?>
 
--- lib/php/class.php
+++ lib/php/class.php
@@ -15,11 +15,59 @@ class record{
     $this->phone2 = $ph2;
     $this->email = $email;
     $this->web = $web;
+  } // end construct
+}  //end record class
+
+class Records{
+  
+  public static function hello() {
+    echo '<h1>hello</h1>';
   }
-}
 
+  public static function loadRecords() {
+    if($_FILES[csvDrop]["tmp_name"]){
+      $fileDir = $_FILES[csvDrop]["tmp_name"];
+    } else {
+      $fileDir = $_FILES[csvUpload]["tmp_name"];
+    }
 
+    $row_num = 0;
 
+    ini_set('auto_detect_line_endings', TRUE);
+    if( ($handle = fopen($fileDir, "r")) !== FALSE ) {
+      while( ($row = fgetcsv($handle, 1000, ",")) != FALSE) {
+        if( $row_num == 0 ) {
+          $column_heading = $row;
+          $row_num = 1;
+        } else {
+          $record = array_combine( $column_heading, $row );
+          $records[] = $record;
+        } // end if-else
+      } // end while
+  
+      fclose( $handle );
+    } // end if
+
+    echo '<table cellspacing="0">';
+
+    echo "<tr>";
+    foreach($column_heading as $column){
+      echo "<th>" . $column . "</th>";
+    }
+    echo "</tr>";
+
+    foreach($records as $record) {
+      echo "<tr>";
+      foreach( $record as $key => $value ) {
+        echo "<td>" . $value . "</td>";
+      }
+      echo "</tr>";
+    }
+
+    echo "</table>";
+
+  } // end loadRecords()
+} // end class Records
 
 
 ?>
